587cb903cc6a566fa3afbe5fa769524efe05790d
rrifafauzikomara <rifafauzi6@gmail.com>
2019-10-10 00:31:55 +0700

    add repository test

---
+++ app/src/test/java/com/rifafauzi/moviecatalogue/helper/repository/RepositoryTest.java
@@ -0,0 +1,116 @@
+package com.rifafauzi.moviecatalogue.helper.repository;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.MutableLiveData;
+
+import com.rifafauzi.moviecatalogue.helper.local.LocalDataSource;
+import com.rifafauzi.moviecatalogue.helper.local.entity.MoviesEntity;
+import com.rifafauzi.moviecatalogue.helper.local.entity.TvShowEntity;
+import com.rifafauzi.moviecatalogue.helper.remote.RemoteDataSource;
+import com.rifafauzi.moviecatalogue.helper.vo.Resource;
+import com.rifafauzi.moviecatalogue.model.Movies;
+import com.rifafauzi.moviecatalogue.utils.FakeData;
+import com.rifafauzi.moviecatalogue.utils.InstantAppExecutors;
+import com.rifafauzi.moviecatalogue.utils.LiveDataTestUtil;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RepositoryTest {
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    private LocalDataSource local = mock(LocalDataSource.class);
+    private RemoteDataSource remote = mock(RemoteDataSource.class);
+    private InstantAppExecutors instantAppExecutors = mock(InstantAppExecutors.class);
+    private FakeRepository fakeRepository = new FakeRepository(remote, local, instantAppExecutors);
+
+    private List<Movies> moviesResponses = FakeData.generateDummyApiMovies();
+    private List<MoviesEntity> moviesEntities = FakeData.generateDummyLocalMovies();
+    private int moviesId = moviesResponses.get(0).getId();
+
+    private List<Movies> tvShowResponses = FakeData.generateDummyApiTvShow();
+    private List<TvShowEntity> tvShowEntities = FakeData.generateDummyLocalTvShow();
+    private int tvShowId = tvShowResponses.get(0).getId();
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void getAllMovies() {
+        MutableLiveData<List<MoviesEntity>> listMutableLiveData = new MutableLiveData<>();
+        listMutableLiveData.setValue(moviesEntities);
+
+        when(local.getAllMovies()).thenReturn(listMutableLiveData);
+
+        Resource<List<MoviesEntity>> result = LiveDataTestUtil.getValue(fakeRepository.getAllMovies());
+
+        verify(local).getAllMovies();
+        assertNotNull(result.data);
+        assertEquals(moviesResponses.size(), result.data.size());
+    }
+
+    @Test
+    public void getDetailMovies() {
+        MutableLiveData<MoviesEntity> dummyEntity = new MutableLiveData<>();
+        dummyEntity.setValue(FakeData.getMovieDetail());
+
+        when(local.getDetailMovies(moviesId)).thenReturn(dummyEntity);
+
+        Resource<MoviesEntity> result = LiveDataTestUtil.getValue(fakeRepository.getDetailMovies(moviesId));
+
+        verify(local).getDetailMovies(moviesId);
+        assertNotNull(result.data);
+        assertNotNull(result.data.getTitle());
+        assertEquals(moviesResponses.get(0).getTitle(), result.data.getTitle());
+    }
+
+
+
+    @Test
+    public void getAllTvShow() {
+        MutableLiveData<List<TvShowEntity>> listMutableLiveData = new MutableLiveData<>();
+        listMutableLiveData.setValue(tvShowEntities);
+
+        when(local.getAllTvShow()).thenReturn(listMutableLiveData);
+
+        Resource<List<TvShowEntity>> result = LiveDataTestUtil.getValue(fakeRepository.getAllTvShow());
+
+        verify(local).getAllTvShow();
+        assertNotNull(result.data);
+        assertEquals(tvShowResponses.size(), result.data.size());
+    }
+
+    @Test
+    public void getDetailTvShow() {
+        MutableLiveData<TvShowEntity> dummyEntity = new MutableLiveData<>();
+        dummyEntity.setValue(FakeData.getTvShowsDetail());
+
+        when(local.getDetailTvShow(tvShowId)).thenReturn(dummyEntity);
+
+        Resource<TvShowEntity> result = LiveDataTestUtil.getValue(fakeRepository.getDetailTvShow(tvShowId));
+
+        verify(local).getDetailTvShow(tvShowId);
+        assertNotNull(result.data);
+        assertNotNull(result.data.getName());
+        assertEquals(tvShowResponses.get(0).getName(), result.data.getName());
+    }
+
+}
--- app/src/test/java/com/rifafauzi/moviecatalogue/utils/FakeData.java
+++ app/src/test/java/com/rifafauzi/moviecatalogue/utils/FakeData.java
@@ -1,66 +1,120 @@
 package com.rifafauzi.moviecatalogue.utils;
 
+import com.rifafauzi.moviecatalogue.helper.local.entity.MoviesEntity;
+import com.rifafauzi.moviecatalogue.helper.local.entity.TvShowEntity;
 import com.rifafauzi.moviecatalogue.model.Movies;
-import com.rifafauzi.moviecatalogue.model.TvShow;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FakeData {
-    public static List<Movies> generateDummyMovies() {
+
+    public static List<MoviesEntity> generateDummyLocalMovies() {
+        List<MoviesEntity> moviesEntities = new ArrayList<>();
+        moviesEntities.add(new MoviesEntity(
+                384018,
+                "Fast & Furious Presents: Hobbs & Shaw",
+                "A spinoff of The Fate of the Furious, focusing on Johnson's US Diplomatic Security Agent Luke Hobbs forming an unlikely alliance with Statham's Deckard Shaw.",
+                "2019-08-01",
+                null,
+                "/keym7MPn1icW1wWfzMnW3HeuzWU.jpg"
+        ));
+        moviesEntities.add(new MoviesEntity(
+                420818,
+                "The Lion King",
+                "Simba idolises his father, King Mufasa, and takes to heart his own royal destiny. But not everyone in the kingdom celebrates the new cub's arrival. Scar, Mufasa's brother—and former heir to the throne—has plans of his own. The battle for Pride Rock is ravaged with betrayal, tragedy and drama, ultimately resulting in Simba's exile. With help from a curious pair of newfound friends, Simba will have to figure out how to grow up and take back what is rightfully his.",
+                "2019-07-12",
+                null,
+                "/dzBtMocZuJbjLOXvrl4zGYigDzh.jpg"
+        ));
+        return moviesEntities;
+    }
+
+    public static List<Movies> generateDummyApiMovies() {
         List<Movies> movies = new ArrayList<>();
         movies.add(new Movies(
                 "/dzBtMocZuJbjLOXvrl4zGYigDzh.jpg",
                 "Simba idolises his father, King Mufasa, and takes to heart his own royal destiny. But not everyone in the kingdom celebrates the new cub's arrival. Scar, Mufasa's brother—and former heir to the throne—has plans of his own. The battle for Pride Rock is ravaged with betrayal, tragedy and drama, ultimately resulting in Simba's exile. With help from a curious pair of newfound friends, Simba will have to figure out how to grow up and take back what is rightfully his.",
                 "2019-07-12",
+                "2019-07-12",
                 420818,
+                "The Lion King",
                 "The Lion King"));
         movies.add(new Movies(
                 "/rjbNpRMoVvqHmhmksbokcyCr7wn.jpg",
                 "Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.",
                 "2019-06-28",
+                "2019-06-28",
                 429617,
+                "Spider-Man: Far from Home",
                 "Spider-Man: Far from Home"
         ));
         return movies;
     }
 
-    public static Movies getMovieDetail() {
-        return new Movies(
-                "/dzBtMocZuJbjLOXvrl4zGYigDzh.jpg",
+    public static MoviesEntity getMovieDetail() {
+        return new MoviesEntity(
+                420818,
+                "The Lion King",
                 "Simba idolises his father, King Mufasa, and takes to heart his own royal destiny. But not everyone in the kingdom celebrates the new cub's arrival. Scar, Mufasa's brother—and former heir to the throne—has plans of his own. The battle for Pride Rock is ravaged with betrayal, tragedy and drama, ultimately resulting in Simba's exile. With help from a curious pair of newfound friends, Simba will have to figure out how to grow up and take back what is rightfully his.",
                 "2019-07-12",
-                420818,
-                "The Lion King"
+                null,
+                "/dzBtMocZuJbjLOXvrl4zGYigDzh.jpg"
         );
     }
 
-    public static List<TvShow> generateDummyTvShow() {
-        List<TvShow> tvShows = new ArrayList<>();
-        tvShows.add(new TvShow(
+    public static List<TvShowEntity> generateDummyLocalTvShow() {
+        List<TvShowEntity> tvShowEntities = new ArrayList<>();
+        tvShowEntities.add(new TvShowEntity(
+                60735,
+                "The Flash",
+                "After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only \\\"meta-human\\\" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash.",
+                "2014-10-07",
+                null,
+                "/fki3kBlwJzFp8QohL43g9ReV455.jpg"
+        ));
+        tvShowEntities.add(new TvShowEntity(
+                60735,
+                "The Simpsons",
+                "After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only \\\"meta-human\\\" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash.",
+                "2014-10-07",
+                null,
+                "/fki3kBlwJzFp8QohL43g9ReV455.jpg"
+        ));
+        return tvShowEntities;
+    }
+
+    public static List<Movies> generateDummyApiTvShow() {
+        List<Movies> tvShows = new ArrayList<>();
+        tvShows.add(new Movies(
                 "/b71BaRjp9TwxUZodLGgSRIlkfL8.jpg",
                 "The dramatisation of one of the most notorious killing sprees in British history.",
                 "",
+                "",
                 11634,
+                "The Flash",
                 ""
         ));
-        tvShows.add(new TvShow(
+        tvShows.add(new Movies(
                 "/MoEKaPFHABtA1xKoOteirGaHl1.jpg",
                 "To carry out the biggest heist in history, a mysterious man called The Professor recruits a band of eight robbers who have a single characteristic: none of them has anything to lose. Five months of seclusion - memorizing every step, every detail, every probability - culminate in eleven days locked up in the National Coinage and Stamp Factory of Spain, surrounded by police forces and with dozens of hostages in their power, to find out whether their suicide wager will lead to everything or nothing.",
                 "",
+                "",
                 71446,
+                "",
                 ""
         ));
         return tvShows;
     }
 
-    public static TvShow getTvShowsDetail() {
-        return new TvShow(
-                "/MoEKaPFHABtA1xKoOteirGaHl1.jpg",
-                "To carry out the biggest heist in history, a mysterious man called The Professor recruits a band of eight robbers who have a single characteristic: none of them has anything to lose. Five months of seclusion - memorizing every step, every detail, every probability - culminate in eleven days locked up in the National Coinage and Stamp Factory of Spain, surrounded by police forces and with dozens of hostages in their power, to find out whether their suicide wager will lead to everything or nothing.",
-                "",
-                71446,
-                ""
+    public static TvShowEntity getTvShowsDetail() {
+        return new TvShowEntity(
+                60735,
+                "The Flash",
+                "After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only \\\"meta-human\\\" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash.",
+                "2014-10-07",
+                null,
+                "/fki3kBlwJzFp8QohL43g9ReV455.jpg"
         );
     }
 }
